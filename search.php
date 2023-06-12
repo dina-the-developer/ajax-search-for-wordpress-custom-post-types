f9429a6487676b5a768b798df079341f539eb77b
Dinakaran Kannadhasan <dinakaran.kannadhasan@gmail.com>
2023-06-12 14:04:54 +0530

    Update search.php

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <!-- Add this to your search.php or create a search.php file in your theme -->
 <form class="search-form" method="post" action="">
-  <input type="search" class="search-input" name="s" placeholder="Search" />
+  <input type="search" id="search-input" class="search-input" name="s" placeholder="Search" />
   <input type="submit" value="Search" />
 </form>
 
